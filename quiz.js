e09b79daf6dbab36235f54347fc86143d31a2f8b
Henrique Nicolini <h.nicolini1@gmail.com>
2025-02-08 19:49:19 -0300

    Quiz funcional

--- quiz.js
+++ quiz.js
@@ -3,7 +3,19 @@ const botãocerto = document.getElementById("btn_v");
 const botãoerrado = document.getElementById("btn");
 const botãoerrado2 = document.getElementById("btn2");
 const botãoerrado3 = document.getElementById("btn3");
-
+function redirecionar2() {
+    if (podeRedirecionar) {
+        window.location.href = "quiz2.html";
+    }
+}
+function redirecionar1(){
+    window.location.href = "quiz.html";
+}
+function redirecionar3() {
+    if (podeRedirecionar) {
+        window.location.href = "quiz3.html";
+    }
+}
 
 function correto() {
     botãocerto.style.backgroundColor = "green";
@@ -17,17 +29,21 @@ function errado() {
 }
 
 
+let podeRedirecionar = false;
+
+function habilitarRedirecionamento() {
+    podeRedirecionar = true;
+}
 
 botãocerto.addEventListener('click', correto);
-
 botãoerrado.addEventListener('click', errado);
 botãoerrado2.addEventListener('click', errado);
+botãoerrado3.addEventListener('click', errado);
 
-if (botãocerto.style.backgroundColor == "green"){
-    getElementById('but_2').addEventListener('click', );
-    
 
+const pagina2 = document.getElementById('but_2');
+
+if (podeRedirecionar == true) {
+    pagina2.addEventListener('click', redirecionar);
 }
-function pagina2() {
-        window.location.href = "quiz2.html";
-    }
+
